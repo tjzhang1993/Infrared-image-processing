67844fc4a255e324c8070762c0f481ca4b42d218
tjzhang1993 <46964265+tjzhang1993@users.noreply.github.com>
2021-10-01 15:01:36 -0600

    Add files via upload

---
+++ OTSU_main.m
@@ -0,0 +1,22 @@
+%% OTSU
+[file_name, pathname] = uigetfile( '*.*','????????????');
+I = imread([pathname,'/',file_name]); 
+
+level = graythresh(I);
+BW = im2bw(I,level);
+imshow(BW)
+%% multi-OTSU
+clc;
+clear;
+pathname=uigetdir('*.*','select a dir');
+imgdir=dir([pathname,'/*.jpg']);
+for i=1:length(imgdir)
+    img=imread([pathname,'/',imgdir(i).name]);
+level = graythresh(img);
+h=figure(i);
+BW = im2bw(img,level);
+imshow(BW)
+saveas(h,[pathname,'/OTSU/',imgdir(i).name])     %save
+   close(i)            %close figure
+
+end
